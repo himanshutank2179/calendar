4f15af47ed648f36ad7deaf59068b663f77b2dfa
himanshu <himanshutank1111@gmail.com>
2018-07-13 10:49:17 +0530

    initial commit from himanshu

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+		
+    $('#calendar').fullCalendar({
+        header:false,       
+        defaultView: 'agendaWeek',
+        editable: true,
+        height:"parent",
+        scrollTime:'09:00:00',
+  
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2014-06-01'
+            },
+            {
+                title: 'Long Event',
+                start: '2014-06-07',
+                end: '2014-06-10'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2014-06-09T16:00:00'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2014-06-16T16:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2014-06-12T10:30:00',
+                end: '2014-06-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2014-06-12T12:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2014-06-13T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2014-06-28'
+            }
+        ]
+    });
+    
+});
